a15ab212b8512fd903f6eb7f3eed199793612a3d
Kospio <mikelcarpio97@hotmail.com>
2021-01-30 00:44:53 +0100

    SmoothCamera start

---
+++ Assets/Scrips/CameraFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float FollowSpeed = 2;
+    public Transform Target;
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+    private 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Target.transform.position.x,Target.transform.position.y, transform.position.z);
+    }
+}
