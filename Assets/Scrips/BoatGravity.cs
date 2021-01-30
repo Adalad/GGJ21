570dc5759a57c00dac68ccbafb9e2b1fab13d853
Adalad <8684790+Adalad@users.noreply.github.com>
2021-01-30 10:44:42 +0100

    Boat gravity finished

--- Assets/Scrips/BoatGravity.cs
+++ Assets/Scrips/BoatGravity.cs
@@ -12,13 +12,14 @@ public class BoatGravity : MonoBehaviour
     [Range(0.0f, 100)]
     public float MaxGravity = 5.0f;
     // Lift force
-    [Range(0.0f, 100)]
+    [Range(0.0f, 1000)]
     public float LiftForce = 5.0f;
     // Anchor distance
     [Range(0.0f, 100)]
     public float AnchorDistance = 5.0f;
 
     private Rigidbody2D RigidBodyComponent;
+    private SpriteRenderer SpriteComponent;
     private Vector2 LookDirection;
     private float LookAngle;
 
@@ -30,6 +31,7 @@ public class BoatGravity : MonoBehaviour
     void Start()
     {
         RigidBodyComponent = GetComponent<Rigidbody2D>();
+        SpriteComponent = GetComponent<SpriteRenderer>();
         GameObject[] planetGOs = GameObject.FindGameObjectsWithTag("Planet");
         PlanetTransforms = new Transform[planetGOs.Length];
         for (int i = 0; i < planetGOs.Length; ++i)
@@ -44,11 +46,12 @@ public class BoatGravity : MonoBehaviour
     {
         if (AnchoredPlanetTransform != null)
         {
+            int direction = (AnchoredPlanetTransform.GetComponent<PlanetRotation>().RotationSpeed > 0) ? -1 : 1;
             LastPlanetTransform = AnchoredPlanetTransform;
             AnchoredPlanetTransform = null;
             RigidBodyComponent.isKinematic = false;
             transform.parent = null;
-            RigidBodyComponent.AddForce(transform.up.normalized * LiftForce);
+            RigidBodyComponent.AddForce(transform.right.normalized * LiftForce * direction);
 
             StartCoroutine(Gravitate());
         }
@@ -65,23 +68,37 @@ public class BoatGravity : MonoBehaviour
                     // Distance to the planet
                     float dist = Vector3.Distance(PlanetTransforms[i].position, transform.position);
 
-                    // Gravity
-                    Vector3 v = PlanetTransforms[i].position - transform.position;
-                    RigidBodyComponent.AddForce(v.normalized * (1.0f - dist / MaxGravityDistance) * MaxGravity);
-
-                    // Rotating to the planet
-                    LookDirection = PlanetTransforms[i].position - transform.position;
-                    LookAngle = Mathf.Atan2(LookDirection.y, LookDirection.x) * Mathf.Rad2Deg;
-
-                    transform.rotation = Quaternion.Euler(0f, 0f, LookAngle);
-
-                    if (dist <= AnchorDistance)
+                    if (dist < MaxGravityDistance)
                     {
-                        AnchoredPlanetTransform = PlanetTransforms[i];
-                        RigidBodyComponent.isKinematic = true;
-                        RigidBodyComponent.velocity = Vector2.zero;
-                        transform.parent = AnchoredPlanetTransform;
-                        break;
+                        // Gravity
+                        Vector3 v = PlanetTransforms[i].position - transform.position;
+                        RigidBodyComponent.AddForce(v.normalized * (1.0f - dist / MaxGravityDistance) * MaxGravity);
+
+                        // Rotating to the planet
+                        LookDirection = PlanetTransforms[i].position - transform.position;
+                        LookAngle = 90 + Mathf.Atan2(LookDirection.y, LookDirection.x) * Mathf.Rad2Deg;
+                        float lerpRatio = 1 - ((dist - AnchorDistance) / (MaxGravityDistance - AnchorDistance));
+                        Vector3 newRotation = transform.eulerAngles;
+                        newRotation.z = LookAngle * lerpRatio;
+                        transform.eulerAngles = newRotation;
+
+                        if (dist <= AnchorDistance)
+                        {
+                            AnchoredPlanetTransform = PlanetTransforms[i];
+                            if (AnchoredPlanetTransform.GetComponent<PlanetRotation>().RotationSpeed > 0)
+                            {
+                                SpriteComponent.flipX = true;
+                            }
+                            else
+                            {
+                                SpriteComponent.flipX = false;
+                            }
+
+                            RigidBodyComponent.isKinematic = true;
+                            RigidBodyComponent.velocity = Vector2.zero;
+                            transform.parent = AnchoredPlanetTransform;
+                            break;
+                        }
                     }
                 }
             }
