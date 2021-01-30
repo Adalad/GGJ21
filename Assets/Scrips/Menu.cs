bfd68906cdf9c6fafc9b156c2b497c47a9e9f3dd
Kospio <mikelcarpio97@hotmail.com>
2021-01-30 20:37:48 +0100

    Animación barco + Barco.psb

--- Assets/Scrips/Menu.cs
+++ Assets/Scrips/Menu.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    GameObject Camera; 
-    float CameraSpeed;
-
     public bool paused = false;
 
-     
+    public float percentaje = 1; 
+
+    Vector3 DestinationCameraMenu;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        DestinationCameraMenu = new Vector3(0, 0, -10);
     }
 
     // Update is called once per frame
@@ -22,19 +21,33 @@ public class Menu : MonoBehaviour
     {
         if (Input.GetKeyDown("f") && paused == false)
         {
-            OpenMap();
-            paused = true; 
+            paused = true;
+
+            Debug.Log(paused);
+
+            StartCoroutine(MenuSmooth());
         }
 
-        if (Input.GetKeyDown("f") && paused == true)
-        {            
-            paused = false;
+        if (transform.position != DestinationCameraMenu && paused == true)
+        {
+            transform.position = Vector3.MoveTowards(this.transform.position, DestinationCameraMenu, 1f);
+
+            if(Input.GetKeyDown("f") && paused == false)
+            {
+                paused = false;
+            }
         }
     }
 
-    void OpenMap()
+    IEnumerator MenuSmooth()
     {
-        Debug.Log("f"); 
-        Vector3.MoveTowards(transform.position, new Vector3(0, 0, transform.position.z), CameraSpeed); 
+        while (Camera.main.orthographicSize < 65)
+        {
+            Camera.main.orthographicSize += (0.01f * percentaje); //Esto intenta hacer el cambio de camara de forma smooth, no funciona al parecer
+
+            percentaje = Camera.main.orthographicSize * 65 / 100;
+
+            yield return null;
+        }
     }
 }
